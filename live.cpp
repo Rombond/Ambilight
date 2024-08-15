3c0a4d4c6aa8b91f5aeb49ccb4a7dfbd96e63838
Rombond <rombond@live.fr>
2024-08-15 18:04:31 -0400

    adding rgb input

--- live.cpp
+++ live.cpp
@@ -9,7 +9,10 @@
 #include <sys/ioctl.h>
 #include <string.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/ocl.hpp>
 #include <csignal>
+
+#define VERBOSE 0
 
 #define VTYTPE V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE
 const unsigned int BUFFER_COUNT = 4;
@@ -22,7 +25,9 @@ struct bufSt{
 int bufferIndex = 0;
 unsigned int width = 3840;
 unsigned int height = 2160;
+unsigned int format = V4L2_PIX_FMT_NV12;
 bool loop = true;
+cv::Mat bgr_output;
 
 const char* fmt2str( unsigned fmt )
 {
@@ -53,7 +58,9 @@ int request_buffers(int fd) {
         printf("Errno: %d\n", errno);
         return ret;
     }
+    #if VERBOSE
     printf("VIDIOC_REQBUFS ok %d alloued\n", req.count);
+    #endif
     buffers = static_cast<bufSt*>(calloc(req.count, sizeof(bufSt)));
     for (size_t i = 0; i < req.count; ++i) {
         v4l2_buffer buf = v4l2_buffer();
@@ -68,7 +75,9 @@ int request_buffers(int fd) {
             perror("VIDIOC_QUERYBUF");
             return ret;
         }
+        #if VERBOSE
         printf("VIDIOC_QUERYBUF ok\n");
+        #endif
         for (size_t j = 0; j < PLANE_COUNT; ++j) {
             buffers[i].length[j] = buf.m.planes[j].length;
             buffers[i].start[j] = mmap(NULL /* start anywhere */,
@@ -81,7 +90,9 @@ int request_buffers(int fd) {
                 printf("Errno: %d\n", errno);
                 return -EXIT_FAILURE;
             }
+            #if VERBOSE
             printf("mmap ok\n");
+            #endif
         }
     }
     return 0;
@@ -102,7 +113,9 @@ int queue_buffer(int fd, int index) {
         perror("VIDIOC_QBUF");
         return ret;
     }
+    #if VERBOSE
     printf("VIDIOC_QBUF ok\n");
+    #endif
     return ret;
 }
 
@@ -121,7 +134,9 @@ int dequeue_buffer(int fd, int fb) {
         perror("VIDIOC_DQBUF");
         return ret;
     }
+    #if VERBOSE
     printf("VIDIOC_DQBUF ok\n");
+    #endif
     bufferIndex = buf.index;
     return ret;
 }
@@ -136,12 +151,27 @@ int stop_capture(int fd) {
     return ioctl(fd, VIDIOC_STREAMOFF, &type);
 }
 
-int process_frame(int fb, size_t index) {    
-    cv::Mat yuv(height*1.5, width, CV_8UC1, buffers[index].start[0]);
-    cv::Mat bgr(height, width, CV_8UC4);
-    cv::cvtColor(yuv, bgr, cv::COLOR_YUV2BGRA_NV12);
-    lseek(fb, 0, SEEK_SET);
-    write(fb, bgr.data, bgr.total() * bgr.elemSize());
+int process_frame(int fb, size_t index) {
+    if (format == V4L2_PIX_FMT_NV12) {
+        cv::Mat yuv_input(height * 1.5, width, CV_8UC1, buffers[index].start[0]);
+        cv::UMat yuv = yuv_input.clone().getUMat(cv::ACCESS_READ);
+        cv::UMat bgr;
+        // La conversion de couleur est automatiquement accélérée par OpenCL si activé
+        cv::cvtColor(yuv, bgr, cv::COLOR_YUV2BGRA_NV12);
+        // Écriture des données converties dans le framebuffer
+        bgr.getMat(cv::ACCESS_READ).copyTo(bgr_output);
+        lseek(fb, 0, SEEK_SET);
+        write(fb, bgr_output.data, bgr_output.cols * bgr_output.rows * bgr_output.channels() * sizeof(unsigned char));
+    } else {
+        cv::Mat yuv_input(height, width, CV_8UC3, buffers[index].start[0]);
+        if (bgr_output.empty() || bgr_output.cols != width || bgr_output.rows != height || bgr_output.type() != CV_8UC4) {
+            bgr_output.create(height, width, CV_8UC4); // CV_8UC4 pour BGRA
+        }
+        cv::cvtColor(yuv_input, bgr_output, cv::COLOR_BGR2BGRA);
+        lseek(fb, 0, SEEK_SET);
+        write(fb, bgr_output.data, bgr_output.cols * bgr_output.rows * bgr_output.channels() * sizeof(unsigned char));
+    }
+
     return EXIT_SUCCESS;
 }
 
@@ -173,6 +203,12 @@ void signalHandler( int signum ) {
 }
 
 int main() {
+    if(cv::ocl::haveOpenCL()) {
+        cv::ocl::setUseOpenCL(true);
+        std::cout << "OpenCL is enabled in OpenCV" << std::endl;
+    } else {
+        std::cout << "OpenCL is not available" << std::endl;
+    }
     signal(SIGINT, signalHandler); 
 
     int fd = open("/dev/video0", O_RDWR | O_NONBLOCK, 0);
@@ -197,14 +233,16 @@ int main() {
     }
     width = fmt.fmt.pix_mp.width;
     height = fmt.fmt.pix_mp.height;
-    if (1 == 1) {
-        printf("width: %d\n", width);
-        printf("height: %d\n", height);
-        printf("pixelformat: %s\n", fmt2str(fmt.fmt.pix_mp.pixelformat));
-        printf("field: %d\n", fmt.fmt.pix_mp.field);
-        printf("colorspace: %d\n", fmt.fmt.pix_mp.colorspace);
-        printf("num_planes: %d\n", fmt.fmt.pix_mp.num_planes);
-    }
+    format = fmt.fmt.pix_mp.pixelformat;
+    printf("pixelformat: %s\n", fmt2str(fmt.fmt.pix_mp.pixelformat));
+    #if VERBOSE
+    printf("width: %d\n", width);
+    printf("height: %d\n", height);
+    printf("pixelformat: %s\n", fmt2str(fmt.fmt.pix_mp.pixelformat));
+    printf("field: %d\n", fmt.fmt.pix_mp.field);
+    printf("colorspace: %d\n", fmt.fmt.pix_mp.colorspace);
+    printf("num_planes: %d\n", fmt.fmt.pix_mp.num_planes);
+    #endif
 
     ret = request_buffers(fd);
     if (ret < 0) perror("request_buffers");
@@ -234,7 +272,9 @@ int main() {
             perror("dequeue_buffer");
             break;
         }
+        auto start = std::chrono::high_resolution_clock::now();
         process_frame(fb, bufferIndex);
+        auto elapsed = std::chrono::high_resolution_clock::now() - start;
         if (ret < 0) {
             perror("process_frame");
             break;
@@ -244,6 +284,8 @@ int main() {
             perror("queue_buffer");
             break;
         }
+        long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+        printf("%ld\n", microseconds);
     }
 
     stop_capture(fd);
