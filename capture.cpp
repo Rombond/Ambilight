326fed96a8d1cb305adf13721b793f094dd6837c
Rombond <rombond@live.fr>
2024-08-15 18:32:09 -0400

    clean

--- capture.cpp
+++ capture.cpp
@@ -9,12 +9,20 @@
 #include <sys/ioctl.h>
 #include <string.h>
 
+#define VERBOSE 0
+
 #define VTYTPE V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE
-const unsigned int PLANE_COUNT = 1;
-const unsigned int BUFFER_COUNT = 2;
-void* buffersPtr[BUFFER_COUNT][PLANE_COUNT];
-struct v4l2_plane planes[BUFFER_COUNT][PLANE_COUNT];
-struct v4l2_buffer buffers[BUFFER_COUNT];
+struct bufSt{
+    void *start[VIDEO_MAX_PLANES];
+    size_t length[VIDEO_MAX_PLANES];
+} *buffers;
+
+int bufferIndex = 0;
+unsigned int width = 3840;
+unsigned int height = 2160;
+unsigned int format = V4L2_PIX_FMT_NV12;
+unsigned int num_planes = VIDEO_MAX_PLANES;
+unsigned int num_buffers = 4;
 
 const char* fmt2str( unsigned fmt )
 {
@@ -31,13 +39,13 @@ int get_format(int fd, struct v4l2_format *fmt) {
     return ioctl(fd, VIDIOC_G_FMT, fmt);
 }
 
-int request_buffers(int fd, uint32_t width, uint32_t height) {
+int request_buffers(int fd) {
     struct v4l2_requestbuffers req;
     memset(&req, 0, sizeof(req));
     req.type = VTYTPE;
     req.memory = V4L2_MEMORY_MMAP;
     req.flags = 0;
-    req.count = BUFFER_COUNT;
+    req.count = num_buffers;
 
     int ret = ioctl(fd, VIDIOC_REQBUFS, &req);
     if (ret < 0) {
@@ -45,54 +53,88 @@ int request_buffers(int fd, uint32_t width, uint32_t height) {
         printf("Errno: %d\n", errno);
         return ret;
     }
-    printf("VIDIOC_REQBUFS ok %d alloued\n", req.count);
-    for (size_t i = 0; i < req.count; ++i) {
-
-        memset(&buffers[i], 0, sizeof(buffers[i]));
-        memset(&planes[i], 0, sizeof(planes[i]));
-        buffers[i].type = VTYTPE;
-        buffers[i].memory = V4L2_MEMORY_MMAP;
-        buffers[i].index = i;
-        buffers[i].m.planes = planes[i];
-        buffers[i].length = PLANE_COUNT;
-        ret = ioctl(fd, VIDIOC_QUERYBUF, &buffers[i]);
+    num_buffers = req.count;
+    #if VERBOSE
+    printf("VIDIOC_REQBUFS ok %d alloued\n", num_buffers);
+    #endif
+    buffers = static_cast<bufSt*>(calloc(num_buffers, sizeof(bufSt)));
+    for (size_t i = 0; i < num_buffers; ++i) {
+        v4l2_buffer buf = v4l2_buffer();
+        v4l2_plane mplanes[VIDEO_MAX_PLANES];
+        buf.type = VTYTPE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        buf.m.planes = mplanes;
+        buf.length = VIDEO_MAX_PLANES;
+        ret = ioctl(fd, VIDIOC_QUERYBUF, &buf);
         if (ret < 0) {
             perror("VIDIOC_QUERYBUF");
             return ret;
         }
+        #if VERBOSE
         printf("VIDIOC_QUERYBUF ok\n");
-        for (size_t j = 0; j < PLANE_COUNT; ++j) {
-            buffersPtr[i][j] = mmap(NULL, buffers[i].m.planes[j].length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buffers[i].m.planes[j].m.mem_offset);
-            if (buffersPtr[i][j] == MAP_FAILED) {
+        #endif
+        num_planes = buf.length;
+        for (size_t j = 0; j < num_planes; ++j) {
+            buffers[i].length[j] = buf.m.planes[j].length;
+            buffers[i].start[j] = mmap(NULL /* start anywhere */,
+                     buffers[i].length[j],
+                     PROT_READ /* required */,
+                     MAP_SHARED /* recommended */,
+                     fd, buf.m.planes[j].m.mem_offset);
+            if (buffers[i].start == MAP_FAILED) {
                 perror("mmap");
                 printf("Errno: %d\n", errno);
                 return -EXIT_FAILURE;
             }
+            #if VERBOSE
             printf("mmap ok\n");
+            #endif
         }
     }
     return 0;
 }
 
 int queue_buffer(int fd, int index) {
+    v4l2_buffer buf = v4l2_buffer();
+    v4l2_plane mplanes[VIDEO_MAX_PLANES];
+    buf.type = VTYTPE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = index;
+    buf.m.planes = mplanes;
+    buf.length = VIDEO_MAX_PLANES;
+
     int ret = -1;
-    ret = ioctl(fd, VIDIOC_QBUF, &buffers[index]);
+    ret = ioctl(fd, VIDIOC_QBUF, &buf);
     if (ret < 0) {
         perror("VIDIOC_QBUF");
         return ret;
     }
+    #if VERBOSE
     printf("VIDIOC_QBUF ok\n");
+    #endif
     return ret;
 }
 
 int dequeue_buffer(int fd) {
+    v4l2_buffer buf = v4l2_buffer();
+    v4l2_plane mplanes[VIDEO_MAX_PLANES];
+    buf.type = VTYTPE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = 0;
+    buf.m.planes = mplanes;
+    buf.length = VIDEO_MAX_PLANES;
+
     int ret = -1;
-    ret = ioctl(fd, VIDIOC_DQBUF, &buffers[0]);
+    ret = ioctl(fd, VIDIOC_DQBUF, &buf);
     if (ret < 0) {
         perror("VIDIOC_DQBUF");
         return ret;
     }
+    #if VERBOSE
     printf("VIDIOC_DQBUF ok\n");
+    #endif
+    bufferIndex = buf.index;
     return ret;
 }
 
@@ -107,48 +149,44 @@ int stop_capture(int fd) {
 }
 
 int process_frame(size_t index) {
-    printf("index: %d\n", index);
-    printf("NbPlanes: %d\n", buffers[index].length);
-    printf("plane bytesused: %d\n", buffers[index].m.planes[0].bytesused);
-    printf("plane length: %d\n", buffers[index].m.planes[0].length);
-    printf("plane data_offset: %u\n", buffers[index].m.planes[0].data_offset);
-    printf("plane mem_offset: %u\n", buffers[index].m.planes[0].m.mem_offset);
-    int file = open("output.yuv", O_WRONLY | O_CREAT, 0666);
-	for(size_t j = 0; j < PLANE_COUNT; ++j){
-        ssize_t written = write(file, buffersPtr[index][j], buffers[index].m.planes[j].bytesused); // Write each plane to the file
-        if (written == -1) {
-            perror("write");
-            close(file);
-            return -EXIT_FAILURE;
-        }
-    }
+    //Image is at `buffers[index].start[j]`
+    printf("Got image from buffer %d\n", index);
     return EXIT_SUCCESS;
 }
 
 int cleanup(int fd) {
-    for (size_t i = 0; i < BUFFER_COUNT; ++i) {
-        for(size_t j = 0; j < PLANE_COUNT; ++j){
-            if (munmap(buffersPtr[i][j], buffers[i].m.planes[j].length) == -1) { // Unmap each plane separately
+    for (size_t i = 0; i < num_buffers; ++i) {
+        for(size_t j = 0; j < num_planes; ++j){
+            if (munmap(buffers[i].start[j], buffers[i].length[j]) == -1) { // Unmap each plane separately
                 perror("munmap");
                 return -EXIT_FAILURE;
             }
         }
     }
+    #if VERBOSE
+    printf("munmap ok\n");
+    #endif
 
     if (close(fd) < 0) { // Close the device file descriptor
         perror("close");
         return -EXIT_FAILURE;
     }
+    #if VERBOSE
+    printf("close ok\n");
+    #endif
 
     return EXIT_SUCCESS;
 }
 
 int main() {
-    int fd = open("/dev/video0", O_RDWR, 0);
+    int fd = open("/dev/video0", O_RDWR | O_NONBLOCK, 0);
     if (fd < 0) {
         std::cerr << "Failed to open video device" << std::endl;
         return 1;
     }
+    #if VERBOSE
+    printf("open ok\n");
+    #endif
 
     v4l2_format fmt;
     memset(&fmt, 0, sizeof(fmt));
@@ -159,35 +197,58 @@ int main() {
         perror("get_format");
         return EXIT_FAILURE;
     }
+    #if VERBOSE
+    printf("get_format ok\n");
+    #endif
+    width = fmt.fmt.pix_mp.width;
+    height = fmt.fmt.pix_mp.height;
+    format = fmt.fmt.pix_mp.pixelformat;
+    #if VERBOSE
+    printf("width: %d\n", width);
+    printf("height: %d\n", height);
+    printf("pixelformat: %s\n", fmt2str(fmt.fmt.pix_mp.pixelformat));
+    printf("field: %d\n", fmt.fmt.pix_mp.field);
+    printf("colorspace: %d\n", fmt.fmt.pix_mp.colorspace);
+    printf("num_planes: %d\n", fmt.fmt.pix_mp.num_planes);
+    #endif
 
-    if (1 == 1) {
-        printf("width: %d\n", fmt.fmt.pix_mp.width);
-        printf("height: %d\n", fmt.fmt.pix_mp.height);
-        printf("pixelformat: %s\n", fmt2str(fmt.fmt.pix_mp.pixelformat));
-        printf("field: %d\n", fmt.fmt.pix_mp.field);
-        printf("colorspace: %d\n", fmt.fmt.pix_mp.colorspace);
-        printf("num_planes: %d\n", fmt.fmt.pix_mp.num_planes);
-    }
-
-    ret = request_buffers(fd, fmt.fmt.pix_mp.width, fmt.fmt.pix_mp.height);
+    ret = request_buffers(fd);
     if (ret < 0) perror("request_buffers");
     ret = -1;
-    for(int i = 0; i < BUFFER_COUNT; i++)
+    for(int i = 0; i < num_buffers; i++) {
         ret = queue_buffer(fd, i);
-    if (ret < 0) perror("queue_buffer");
+        if (ret < 0) perror("queue_buffer");
+    }
     ret = start_capture(fd);
     if (ret < 0) perror("start_capture");
 
-    int index = dequeue_buffer(fd);
-    if (index < 0) {
-        perror("queue_buffer");
-        return EXIT_FAILURE;
-    } 
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+    struct timeval tv = {0};
+    tv.tv_sec = 2;
+    int r;
+    r = select(fd+1, &fds, NULL, NULL, &tv);
+    if(-1 == r){
+        perror("Waiting for Frame");
+        exit(1);
+    }
 
-    process_frame(index);
-    if (ret < 0) perror("process_frame");
-    queue_buffer(fd, index);
-    if (ret < 0) perror("queue_buffer");
+    ret = dequeue_buffer(fd);
+    if (ret < 0) {
+        perror("dequeue_buffer");
+        return cleanup(fd);
+    }
+    ret = process_frame(bufferIndex);
+    if (ret < 0) {
+        perror("process_frame");
+        return cleanup(fd);
+    }
+    queue_buffer(fd, bufferIndex);
+    if (ret < 0) {
+        perror("queue_buffer");
+        return cleanup(fd);
+    }
 
     stop_capture(fd);
     if (ret < 0) perror("stop_capture");
